aec2705d597f19bf433a8a6655f8114046c361cf
Tim <67547385+TheGreenPig@users.noreply.github.com>
2021-09-04 15:57:48 +0200

    Update PdfViewer.plugin.js

--- PdfViewer/PdfViewer.plugin.js
+++ PdfViewer/PdfViewer.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name PdfViewer
  * @author AGreenPig
- * @version 0.0.1
+ * @version 0.0.2
  * @updateUrl https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js
  * @authorLink https://github.com/TheGreenPig
  * @source https://github.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js
@@ -16,7 +16,7 @@ module.exports = (() => {
 				"discord_id": "427179231164760066",
 				"github_username": "TheGreenPig"
 			}],
-			"version": "0.0.1",
+			"version": "0.0.2",
 			"description": "View Pdf files directly in Discord.",
 			"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js"
 		},
@@ -43,34 +43,82 @@ module.exports = (() => {
 		stop() { }
 	} : (([Plugin, Api]) => {
 		const plugin = (Plugin, Library) => {
+			const {
+				Tooltip
+			} = { ...Api, ...BdApi };
 			return class PdfViwer extends Plugin {
-				onStart() {
+
+
+				addPdfButton(element) {
+
+					let pdfButton = document.createElement("a");
+
+					pdfButton.className = "anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB downloadWrapper-vhAtLx"
+
+					pdfButton.innerHTML = `<svg class="downloadButton-23tKQp" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
+											<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z">
+											</path>
+											</svg>`;
+
+					let tooltip = new Tooltip(pdfButton, "Click to view pdf.");
+					pdfButton.addEventListener("click", () => {
+						if (element.classList.contains("showPdf")) {
+							BdApi.injectCSS("pdfViewer", `.pdfDisplay {display:none;}`);
+							BdApi.clearCSS("pdfMessageWrapper")
+							tooltip = new Tooltip(pdfButton, "Click to show pdf.")
+							element.classList.remove("showPdf");
+						} else {
+							BdApi.clearCSS("pdfViewer")
+							BdApi.injectCSS("pdfMessageWrapper", ".showPdf {width: 150%;}")
+							tooltip = new Tooltip(pdfButton, "Click to hide pdf.")
+							element.classList.add("showPdf");
+						}
+
+					});
+
+
+					pdfButton.addEventListener('hover', () => {
+						tooltip.showAbove();
+					});
+					if (element.nodeName === "DIV") {
+						let attachmentWrapper = element.querySelector(".attachment-33OFj0");
+						attachmentWrapper.appendChild(pdfButton);
+					}
+					else {
+						element.appendChild(pdfButton);
+					}
+					element.classList.add("displayPdfButton");
 				}
-				onStop() {
+				showPdf(element, url) {
+					if (element.querySelector(".pdfDisplay")) {
+						return;
+					}
+					let embed = document.createElement("embed");
+					embed.className = `imageWrapper-2p5ogY imageZoom-1n-ADA clickable-3Ya1ho embedMedia-1guQoW embedImage-2W1cML pdfDisplay`;
+					embed.setAttribute("src", `https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`);
+					embed.setAttribute("type", "application/pdf")
+					embed.setAttribute("width", "50%")
+					embed.setAttribute("height", "600rem")
+					BdApi.injectCSS("pdfViewer", `.pdfDisplay {display:none;}`);
+
+					element.appendChild(embed);
 				}
 				observer() {
 					let allAttachments = document.querySelectorAll(".messageAttachment-1aDidq");
-
-					let allLinks = document.querySelectorAll(".anchor-3Z-8Bb.anchorUnderlineOnHover-2ESHQB");
-					if(allAttachments.length>0) {
-						for(let attachment of allAttachments) {
+					if (allAttachments.length > 0) {
+						for (let attachment of allAttachments) {
 							let url = attachment.querySelector("div > a")?.getAttribute("href");
-							if(url && url.endsWith(".pdf")) {
-								attachment.outerHTML = `<embed class=" imageWrapper-2p5ogY imageZoom-1n-ADA clickable-3Ya1ho embedMedia-1guQoW embedImage-2W1cML" src="https://drive.google.com/viewerng/
-								viewer?embedded=true&url=${url}" type="application/pdf" width="500rem" height="600rem">`;
+							if (url && url.endsWith(".pdf")) {
+								if (!attachment.classList.contains("displayPdfButton")) {
+									this.addPdfButton(attachment);
+									this.showPdf(attachment, url)
+								}
 							}
 						}
-					}
-					if(allLinks.length>0) {
-						for(let link of allLinks) {
-							let url = link.getAttribute("href");
-							if(url && url.endsWith(".pdf")) {
-								link.outerHTML = `<embed class=" imageWrapper-2p5ogY imageZoom-1n-ADA clickable-3Ya1ho embedMedia-1guQoW embedImage-2W1cML" src="https://drive.google.com/viewerng/
-								viewer?embedded=true&url=${url}" type="application/pdf" width="500rem" height="600rem">`;
-							}
-						}
+
 					}
 				}
+
 			}
 		};
 		return plugin(Plugin, Api);
