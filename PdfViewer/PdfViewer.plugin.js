8f61a0b131014eb40dcdc5d965afb20bbb5aa55f
Tim <67547385+TheGreenPig@users.noreply.github.com>
2021-09-06 14:32:09 +0200

    Updated Version and changed name to File viwer

--- PdfViewer/PdfViewer.plugin.js
+++ PdfViewer/PdfViewer.plugin.js
@@ -1,25 +1,25 @@
 /**
- * @name PdfViewer
+ * @name FileViewer
  * @author AGreenPig
- * @version 0.0.3
- * @updateUrl https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js
+ * @version 0.0.4
+ * @updateUrl https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/FileViewer/FileViewer.plugin.js
  * @authorLink https://github.com/TheGreenPig
- * @source https://github.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js
+ * @source https://github.com/TheGreenPig/BetterDiscordPlugins/main/FileViewer/FileViewer.plugin.js
  */
 
 
-module.exports = (() => {
+ module.exports = (() => {
 	const config = {
 		"info": {
-			"name": "PdfViewer",
+			"name": "FileViewer",
 			"authors": [{
 				"name": "AGreenPig",
 				"discord_id": "427179231164760066",
 				"github_username": "TheGreenPig"
 			}],
-			"version": "0.0.3",
-			"description": "View Pdf files directly in Discord.",
-			"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/PdfViewer/PdfViewer.plugin.js"
+			"version": "0.0.4",
+			"description": "View Pdf and other files directly in Discord.",
+			"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/FileViewer/FileViewer.plugin.js"
 		},
 	}
 	return !global.ZeresPluginLibrary ? class {
@@ -48,71 +48,68 @@ module.exports = (() => {
 				Tooltip
 			} = { ...Api, ...BdApi };
 
-			let pdfCSS = [
-				`.hidePdf {`,
+			let fileCSS = [
+				`.hideFile {`,
 				`	display: none;`,
 				`}`,
 			].join("\n")
-			return class PdfViwer extends Plugin {
+			return class FileViwer extends Plugin {
 				start() {
-					BdApi.injectCSS("pdfCSS", pdfCSS);
+					BdApi.injectCSS("fileCSS", fileCSS);
 				}
 				stop() {
-					BdApi.clearCSS("pdfCSS");
+					BdApi.clearCSS("fileCSS");
 				}
-				addPdfButton(attachment) {
-					let pdfButton = document.createElement("a");
-					pdfButton.className = "anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB downloadWrapper-vhAtLx"
-					pdfButton.innerHTML = `<svg class="downloadButton-23tKQp" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
+				addFileButton(attachment) {
+					let fileButton = document.createElement("a");
+					fileButton.className = "anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB downloadWrapper-vhAtLx"
+					fileButton.innerHTML = `<svg class="downloadButton-23tKQp" aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
 											<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z">
 											</path>
 											</svg>`;
 
 					let tooltip;
-					pdfButton.addEventListener("click", () => {
-						if (attachment.getAttribute("displayingpdf") === "true") {
-							//hide pdf
-							console.log("hide pdf")
-							tooltip = new Tooltip(pdfButton, "Click to view pdf.");
-							attachment.setAttribute("displayingpdf", "false");
+					fileButton.addEventListener("click", () => {
+						if (attachment.getAttribute("displayingfile") === "true") {
+							//hide file
+							tooltip = new Tooltip(fileButton, "Click to view file.");
+							attachment.setAttribute("displayingFile", "false");
 
-							attachment.querySelector(".pdfEmbed").classList.add("hidePdf");
-							attachment.querySelector(".pdfInfo").classList.add("hidePdf");
+							attachment.querySelector(".fileEmbed").classList.add("hideFile");
+							attachment.querySelector(".fileInfo").classList.add("hideFile");
 						} else {
-							//show pdf 
-							console.log("show pdf")
-							tooltip = new Tooltip(pdfButton, "Click to hide pdf.");
-							attachment.setAttribute("displayingpdf", "true");
+							//show file 
+							tooltip = new Tooltip(fileButton, "Click to hide file.");
+							attachment.setAttribute("displayingFile", "true");
 
-							attachment.querySelector(".pdfEmbed").classList.remove("hidePdf");
-							attachment.querySelector(".pdfInfo").classList.remove("hidePdf");
+							attachment.querySelector(".fileEmbed").classList.remove("hideFile");
+							attachment.querySelector(".fileInfo").classList.remove("hideFile");
 						}
 
 					});
 
 
-					pdfButton.addEventListener('hover', () => {
+					fileButton.addEventListener('hover', () => {
 						tooltip.showAbove();
 					});
 					let attachmentWrapper = attachment.querySelector(".attachment-33OFj0");
-					attachmentWrapper.appendChild(pdfButton);
+					attachmentWrapper.appendChild(fileButton);
 
-					attachment.classList.add("displayPdfButton");
+					attachment.classList.add("displayFileButton");
 				}
-				showPdf(attachment, url) {
+				showFile(attachment, url) {
 
 					let googleUrl = `https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`
 					let embed = document.createElement("embed");
-					embed.className = `imageWrapper-2p5ogY imageZoom-1n-ADA clickable-3Ya1ho embedMedia-1guQoW embedImage-2W1cML pdfEmbed hidePdf`;
+					embed.className = `imageWrapper-2p5ogY imageZoom-1n-ADA clickable-3Ya1ho embedMedia-1guQoW embedImage-2W1cML fileEmbed hideFile`;
 					embed.setAttribute("src", googleUrl);
 					embed.setAttribute("frameborder", `0`);
-					embed.setAttribute("type", "application/pdf")
 					embed.setAttribute("width", "500rem")
 					embed.setAttribute("height", "600rem")
-					attachment.setAttribute("displayingpdf", "false")
+					attachment.setAttribute("displayingFile", "false")
 
 					let infoText = document.createElement("div"); 
-					infoText.className = `colorStandard-2KCXvj size14-e6ZScH description-3_Ncsb formText-3fs7AJ modeDefault-3a2Ph1 pdfInfo hidePdf`;
+					infoText.className = `colorStandard-2KCXvj size14-e6ZScH description-3_Ncsb formText-3fs7AJ modeDefault-3a2Ph1 fileInfo hideFile`;
 					infoText.innerHTML = `If nothing is getting displayed, click 
 										<a class="anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB" title="${googleUrl}" rel="noreferrer noopener" target="_blank" role="button" tabindex="0" href="${googleUrl}">here</a> or wait a little.`
 					attachment.appendChild(infoText);
@@ -125,9 +122,9 @@ module.exports = (() => {
 						for (let attachment of allAttachments) {
 							let url = attachment.querySelector("div > a")?.getAttribute("href");
 							if (url && /\.(pdf|doc|docx|xlsx|pptx)$/g.test(url)) {
-								if (!attachment.classList.contains("displayPdfButton")) {
-									this.showPdf(attachment, url)
-									this.addPdfButton(attachment);
+								if (!attachment.classList.contains("displayFileButton")) {
+									this.showFile(attachment, url)
+									this.addFileButton(attachment);
 								}
 							}
 						}
