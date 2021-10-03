773f78575062130de0b525631fb8f5e40d40e250
TheGreenPig <67547385+TheGreenPig@users.noreply.github.com>
2021-10-03 02:21:46 +0200

    Add button support

--- CopyWithFormatting/CopyWithFormatting.plugin.js
+++ CopyWithFormatting/CopyWithFormatting.plugin.js
@@ -1,11 +1,13 @@
 /**
  * @name CopyWithFormatting
  * @author AGreenPig
- * @version 0.0.1
+ * @version 0.0.2
  * @updateUrl https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/CopyWithFormatting/CopyWithFormatting.plugin.js
  * @authorLink https://github.com/TheGreenPig
  * @source https://github.com/TheGreenPig/BetterDiscordPlugins/main/FileViewer/FileViewer.plugin.js
  */
+
+const { appendFile } = require("fs");
 
 
 module.exports = (() => {
@@ -17,8 +19,8 @@ module.exports = (() => {
 				"discord_id": "427179231164760066",
 				"github_username": "TheGreenPig"
 			}],
-			"version": "0.0.1",
-			"description": "Right click to copy the message with correct discord formatting.",
+			"version": "0.0.2",
+			"description": "Right click, or shift-hover over a message to copy the message with correct discord formatting.",
 			"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/CopyWithFormatting/CopyWithFormatting.plugin.js"
 		},
 	}
@@ -51,6 +53,8 @@ module.exports = (() => {
 				start() {
 					const MenuItem = BdApi.findModuleByProps("MenuItem");
 					const MessageContextMenu = BdApi.findModule((m) => m?.default?.displayName === "MessageContextMenu");
+					const MiniPopover = BdApi.findModule((m) => m?.default?.displayName === "MiniPopover");
+					const TooltipWrapper = BdApi.findModuleByPrototypes("renderTooltip");
 					//Credit to p0rtL#6592 for the snippet
 					BdApi.Patcher.after("MessageContextMenuPatch", MessageContextMenu, "default", (that, args, value) => {
 						const [props] = args;
@@ -63,6 +67,52 @@ module.exports = (() => {
 								BdApi.showToast("Copied message!", { type: "success" });
 							}
 						}))
+					});
+					BdApi.Patcher.after("MessageContextMenuPatch", MiniPopover, "default", (that, args, value) => {
+						const [props] = args;
+						const popover = props.children[1].props;
+						if (!popover.expanded) return;
+						let colorText = window.getComputedStyle(document.querySelector(".icon-3Gkjwa"), null).getPropertyValue('color').replace(/\s/g, "");
+						let colorArray = colorText.match(/(\d+),(\d+),(\d+),?(\d+\.\d+)?/);
+						let hex = componentToHex(parseInt(colorArray[1])) + componentToHex(parseInt(colorArray[2])) + componentToHex(parseInt(colorArray[3]));
+						let alpha = 1;
+						function componentToHex(c) {
+							var hex = c.toString(16);
+							return hex.length == 1 ? "0" + hex : hex;
+						}
+						if (typeof colorArray[4] != 'undefined') {
+							alpha = colorArray[4];
+						}
+						const InfoButton = BdApi.React.createElement(TooltipWrapper, {
+							position: TooltipWrapper.Positions.TOP,
+							color: TooltipWrapper.Colors.PRIMARY,
+							text: "Copy message with formatting",
+							children: (tipProps) => {
+								return BdApi.React.createElement("div", Object.assign({
+									children: [
+										BdApi.React.createElement("button", {
+											style: {
+												padding: "4px",
+												marginLeft: "4px",
+												opacity: alpha,
+												background: "none",
+											},
+											children: [
+												BdApi.React.createElement("img", {
+													src: `https://img.icons8.com/material-outlined/24/${hex}/copy.png`,
+												})
+											],
+											onClick: () => {
+												DiscordNative.clipboard.copy(popover.message.content)
+												BdApi.showToast("Copied message!", { type: "success" });
+											}
+										})
+									]
+								}, tipProps))
+							}
+						});
+
+						value.props.children.push(InfoButton);
 					});
 				}
 				stop() {
