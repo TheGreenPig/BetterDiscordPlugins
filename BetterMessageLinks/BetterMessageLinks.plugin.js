fe0519d521d802e6709285211a0eab313a55e3b7
TheGreenPig <67547385+TheGreenPig@users.noreply.github.com>
2021-11-21 02:06:38 +0100

    Better Preview loading, added credits

--- BetterMessageLinks/BetterMessageLinks.plugin.js
+++ BetterMessageLinks/BetterMessageLinks.plugin.js
@@ -12,8 +12,18 @@ const config = {
 			"name": "AGreenPig",
 			"discord_id": "427179231164760066",
 			"github_username": "TheGreenPig"
+		}, {
+			"name": "Strencher",
+			"discord_id": "415849376598982656",
+			"github_username": "Strencher",
+			"twitter_username": "Strencher3"
+		},
+		{
+			"name": "Juby210",
+			"discord_id": "324622488644616195",
+			"github_username": "Juby210"
 		}],
-		"version": "1.0.0",
+		"version": "1.0.1",
 		"description": "Instead of just showing the long and useless discord message link, make it smaller and add a preview.",
 		"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/BetterMessageLinks/BetterMessageLinks.plugin.js"
 	},
@@ -27,6 +37,10 @@ const config = {
  * Zere's Plugin Library documentation:
  * 	 https://rauenzi.github.io/BDPluginLibrary/docs/
 */
+
+
+// This plugin was only made possible by the generous help of Strencher and the MEssageLinkEmbed plugin by him and Juby210! They definitely deserve all the credit
+
 module.exports = !global.ZeresPluginLibrary ? class {
 	constructor() { this._config = config; }
 	getName() { return config.info.name; }
@@ -57,97 +71,141 @@ module.exports = !global.ZeresPluginLibrary ? class {
 	`
 	const defaultSettings = {
 		messageReplaceText: "<Message>",
-		previewMessageLength: 40,
 	};
 
 	//Settings and imports
-	const { Toasts, WebpackModules, DCM, Patcher, React, Settings } = { ...Library, ...BdApi };
+	const { Toasts, WebpackModules, Patcher, React, Settings, DiscordModules } = { ...Library, ...BdApi };
 	const { SettingPanel, Switch, Slider, RadioGroup, Textbox } = Settings;
 
 	//Modules
 	const MessageContent = WebpackModules.getModule(m => m.type?.displayName === "MessageContent");
 	const GetMessageModule = ZLibrary.DiscordModules.MessageStore;
 	const TooltipWrapper = ZLibrary.WebpackModules.getByPrototypes("renderTooltip");
-	const UserMentionModule =  WebpackModules.getByDisplayName("UserMention");
- 
-	//what the message should be replaced with, since the discord urls are long and useless...
+	const UserMentionModule = WebpackModules.getByDisplayName("UserMention");
+	const User = WebpackModules.find(m => m.prototype && m.prototype.tag);
+	const Timestamp = WebpackModules.find(m => m.prototype && m.prototype.toDate && m.prototype.month)
+	const { stringify } = WebpackModules.getByProps('stringify', 'parse', 'encode');
+	let cache = {};
+	let lastFetch = 0;
+
+	async function getMsg(channelId, messageId) {
+		let message = GetMessageModule.getMessage(channelId, messageId) || cache[messageId]
+		if (!message) {
+			if (lastFetch > Date.now() - 2500) await new Promise(r => setTimeout(r, 2500))
+			const data = await DiscordModules.APIModule.get({
+				url: DiscordModules.DiscordConstants.Endpoints.MESSAGES(channelId),
+				query: stringify({
+					limit: 1,
+					around: messageId
+				}),
+				retries: 2
+			})
+			lastFetch = Date.now()
+			message = data.body[0]
+			if (!message) return
+			message.author = new User(message.author)
+			message.timestamp = new Timestamp(message.timestamp)
+		}
+		cache[messageId] = message
+		return message;
+	}
+	const getMsgWithQueue = (() => {
+		let pending = Promise.resolve()
+
+		const run = async (channelId, messageId) => {
+			try {
+				await pending
+			} finally {
+				return getMsg(channelId, messageId)
+			}
+		}
+
+		return (channelId, messageId) => (pending = run(channelId, messageId))
+	})()
+
+	class BetterLink extends React.Component {
+		constructor(props) {
+			super(props)
+			this.state = null;
+		}
+		async componentDidMount() {
+			if (!this.state) {
+				let numberMatches = this.props.original.props.href.match(/\d+/g);
+				let messageId = numberMatches[numberMatches.length - 1];
+				let channelId = numberMatches[numberMatches.length - 2];
+				this.setState(await getMsgWithQueue(channelId, messageId));
+			}
+		}
+		render() {
+			if (!this.state) { return null }
+			let message = this.state;
+			let messageContent = message?.content;
+			let author = message?.author;
+			if (!message?.content || message?.content === "") { return null }
+
+			//replace end with ... if message is long
+			if (messageContent.length > 40) {
+				messageContent = messageContent.substring(0, 40) + "...";
+			}
+			let mention = React.createElement(UserMentionModule, { className: "betterMessageLinks-Mention", userId: author.id });
+
+			//maybe I need this later?
+			// let icon = React.createElement("img", { src: `https://cdn.discordapp.com/avatars/${author.id}/${author.avatar}.webp`, class: "replyAvatar-1K9Wmr betterMessageLinks-Image" })
+
+			//give back the preview
+			let messagePreview = React.createElement("div", { class: "betterMessageLinks-Preview" }, mention, React.createElement("span", { class: "betterMessageLinks-Message" }, " " + messageContent));
+
+			let messageReplace = this.props.original;
+			if (this.props.replace !== "") {
+				messageReplace.props.children[0] = this.props.replace;
+			}
+			//Make the tooltip
+			let newLink = messagePreview ? React.createElement(TooltipWrapper, {
+				position: TooltipWrapper.Positions.TOP,
+				color: TooltipWrapper.Colors.PRIMARY,
+				tooltipClassName: "betterMessageLinks-Tooltip",
+				text: messagePreview,
+				children: (tipProps) => {
+					return React.createElement("span", Object.assign({
+						children: [
+							messageReplace
+						]
+					}, tipProps))
+				}
+			}) : messageReplace; //dont make tooltip if can't get preview
+
+			// return newLink;
+			return newLink
+		}
+	}
 	return class BetterMessageLinks extends Plugin {
 		async onStart() {
 			//inject css
 			BdApi.injectCSS(config.info.name, customCSS)
 			this.settings = this.loadSettings(defaultSettings);
-
 			//add a MessageContent patcher
 			Patcher.after(config.info.name, MessageContent, "type", (_, [props], ret) => {
 				if (ret?.props?.children[0].length > 0) {
 					ret.props.children[0].forEach((child, i) => {
-						if (/https:\/\/(ptb.|canary.)?discord.com\/channels\/\d+\/\d{18}\/\d{18}/gi.test(child.props?.href)) {
-							//a discord link was found!
-							let newLink = ret.props.children[0][i];
-							//messagePreview is the element that gets put into the tooltip
-							let messagePreview = this.getMessagePreview(child);
-							let messageReplace = React.createElement("span", {}, this.settings.messageReplaceText);
-							//Make the tooltip
-							newLink.props.children[0] = messagePreview ? React.createElement(TooltipWrapper, {
-								position: TooltipWrapper.Positions.TOP,
-								color: TooltipWrapper.Colors.PRIMARY,
-								tooltipClassName: "betterMessageLinks-Tooltip",
-								text: messagePreview,
-								children: (tipProps) => {
-									return React.createElement("span", Object.assign({
-										children: [
-											messageReplace
-										]
-									}, tipProps))
-								}
-							}) : messageReplace; //dont make tooltip if can't get preview
-
-							//replace the old link child with the new one
-							ret.props.children[0][i] = newLink;
+						if (/https:\/\/(ptb.|canary.)?discord.com\/channels\/(\d+|@me)\/\d{18}\/\d{18}/gi.test(child.props?.href)) {
+							ret.props.children[0][i] = React.createElement(BetterLink, { original: child, replace: this.settings.messageReplaceText });
 						}
 					});
 				}
 			})
 		}
-		getMessagePreview(child) {
-			//Ideally, i would replace this hand-made preview with the same way
-			//replies get made, so with the icon, name, messagePreview format...
-			let numberMatches = child.props.href.match(/\d+/g);
-			let messageId = numberMatches[2];
-			let channelId = numberMatches[1];
-			let guildId = numberMatches[0];
 
-			//get the message, problem: can't get message when it isn't in MessageStore (other guild, too far up etc.)
-			let message = GetMessageModule.getMessage(channelId, messageId);
-			let messageContent = message?.content;
-			let author = message?.author;
-			if (!message?.content) { return undefined }
-
-			//replace end with ... if message is long
-			if (messageContent.length > this.settings.previewMessageLength) {
-				messageContent = messageContent.substring(0, this.settings.previewMessageLength) + "...";
-			}
-			let mention = React.createElement(UserMentionModule, {className: "mention", userId: author.id});
-			
-			//maybe I need this later?
-			let icon = React.createElement("img", {src: `https://cdn.discordapp.com/avatars/${author.id}/${author.avatar}.webp?size=22`})
-			
-			//give back the preview
-			return React.createElement("span", {}, mention, React.createElement("span", {}, " "+messageContent));
-		}
 		getSettingsPanel() {
-			//build the settings pannel
+			//build the settings panel
 			return SettingPanel.build(() => this.saveSettings(this.settings),
-				new Textbox("Message Replace", "Replace all Discord message links with the following text.", this.settings.messageReplaceText, (i) => {
+				new Textbox("Message Replace", "Replace all Discord message links with the following text. Leave empty to not change the Discord Link at all.", this.settings.messageReplaceText, (i) => {
 					this.settings.messageReplaceText = i;
 				}),
-				//This setting will hopeully not be necessary as soon as I use the built-in Reply element instead...
-				new Slider("Preview character amount", "Defines how many characters the preview should have.", 1, 100, this.settings.previewMessageLength, (i) => {
-					this.settings.previewMessageLength = Math.round(i);
-				}, { markers: [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], stickToMarkers: false, renderValue: (value) => Math.round(value) +" characters" }),
 			)
 
 		}
+
+
 		onStop() {
 			BdApi.clearCSS(config.info.name)
 			Patcher.unpatchAll(config.info.name);
