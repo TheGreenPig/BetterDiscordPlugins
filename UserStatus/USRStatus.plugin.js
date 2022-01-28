8d1415991f7687643ffba70e5174c105d959b183
TheGreenPig <67547385+TheGreenPig@users.noreply.github.com>
2022-01-28 09:53:55 +0100

    Update USRStatus.plugin.js

--- UserStatus/USRStatus.plugin.js
+++ UserStatus/USRStatus.plugin.js
@@ -14,7 +14,7 @@ const config = {
 			"github_username": "TheGreenPig"
 		}],
 		"version": "1.0.0",
-		"description": "A description",
+		"description": "Get an animated status, without abusing the discord api or getting banned.",
 		"github_raw": "https://raw.githubusercontent.com/TheGreenPig/BetterDiscordPlugins/main/UserStatus/USRStatus.plugin.js"
 	},
 }
@@ -53,8 +53,7 @@ module.exports = !global.ZeresPluginLibrary ? class {
 	//Settings and imports
 	const { Toasts, WebpackModules, DCM, Patcher, React, Settings, ReactComponents } = { ...BdApi, ...Library };
 	const { SettingPanel, Switch, Slider, RadioGroup, Textbox } = Settings;
-	let usrstatusJSON;
-	let css;
+	let usrstatusCSS;
 	//default settings
 	const defaultSettings = {
 		radio: 0,
@@ -65,45 +64,65 @@ module.exports = !global.ZeresPluginLibrary ? class {
 			this.settings = this.loadSettings(defaultSettings);
 
 			//This is my test file... later this will be fetched from my github
-			usrstatusJSON = await new Promise((resolve, reject) => {
-				require("fs").readFile(require("path").join(BdApi.Plugins.folder, "usrstatus_test.json"), 'utf8', (err, data) => {
-					if (err) {
-						rejcet(err)
-					}
-					else {
-						resolve(JSON.parse(data))
-					}
-				})
-			})
-			//inject it 
-			BdApi.injectCSS(config.info.name, this.generateAnimationCss(usrstatusJSON))
+			// let usrstatusJSON = await new Promise((resolve, reject) => {
+			// 	require("fs").readFile(require("path").join(BdApi.Plugins.folder, "usrstatus_test.json"), 'utf8', (err, data) => {
+			// 		if (err) {
+			// 			rejcet(err)
+			// 		}
+			// 		else {
+			// 			resolve(JSON.parse(data))
+			// 		}
+			// 	})
+			// })
 
-			//add the class to the status (only in the member list currently)
+			// BdApi.injectCSS(config.info.name, this.generateAnimationCss(usrstatusJSON))
+
+			BdApi.injectCSS(config.info.name, `@import url("https://cdn.jsdelivr.net/gh/TheGreenPig/BetterDiscordPlugins/UserStatus/dist/userstatus_internal.css")`)
+
+			//add the data-user-id calss to the status (member list)
 			const MemberListItem = await ReactComponents.getComponentByName("MemberListItem");
 			Patcher.after(MemberListItem.component.prototype, "render", (_, [props], ret) => {
 				let id = ret.props.children.props.user.id;
-				if (usrstatusJSON[id]) {
-					ret.props.subText.props.className += " status" + id;
-				}
+					ret.props.subText.props.className += ` status${id}`;
 			})
+
+			const UserPopout = WebpackModules.getByProps("UserPopoutCustomStatus");
+			//The id has to passed to the custom status first
+			Patcher.after(UserPopout, "UserPopoutProfileText", (_, [props], ret) => {
+				props.customStatus.props.id = props.user.id;
+			})
+			//add the class
+			Patcher.after(UserPopout, "UserPopoutCustomStatus", (_, [props], ret) => {
+				ret.props.className += ` status${props.id}`; 
+			})
+
+			//need to patch BdApi.findModule(m => m.default.displayName === "UserProfileModalHeader")???
+			/*
+			Places I still need to patch:
+				The friends list
+				The ProfileModal
+			
+			Maybe there is a way to patch the Status component instead so I don't need to have a ton of seperate patchers?
+			*/
 
 		}
 		/*This method is a little complicated and not very intuitive. Essentially it turnes the json file into a usable css file by 
-		calculating the keyframes percentages and bringing everything into correct css syntax.
+		calculating the keyframes percentages and bringing everything into correct css syntax. The internal css file only uses the .status classes,
+		the other one (the bdfdb version) only the attribute selectors. Does this help with performance? I don't know... let's hope so... 
 		In practice I will precomute this and host it on github so the user doesn't have to compute it unnecessarily, so this method
 		is really just for me. An example output for a user would be:
 		
 		Removes the old status
-		.status123456789 > * {
+		.status123456789, .[data-user-id="123456789"] .activity-2EQDZv>*,[data-user-id="123456789"] .customStatus-3XAoF9>*,[data-user-id="123456789"] .customStatus-kFfkj5>* {
 			display: none!important;
 		}
 		Adds the new status
-		.status123456789::before {
+		.status123456789, .[data-user-id="123456789"] .activity-2EQDZv::before,[data-user-id="123456789"] .customStatus-3XAoF9::before,[data-user-id="123456789"] .customStatus-kFfkj5::before {
 			content: "thumbnail status";
-			animation: a123456789 2s linear infinite;
+			animation: frames123456789 2s linear infinite;
 		}
 		Add the keyframes
-		@keyframes a123456789 {
+		@keyframes frames123456789 {
 			0%{content: "First Frame"}
 			25%{content: "Second Frame"}
 			50%{content: "Third Frame"}
@@ -114,7 +133,6 @@ module.exports = !global.ZeresPluginLibrary ? class {
 		generateAnimationCss(list) {
 			let css = ""
 			Object.entries(list).forEach(entry => {
-				console.log(entry);
 				let convertedInstructions = [];
 				let frames = entry[1].frames
 				convertedInstructions.push([0, Object.keys(frames[0])[0]]);
@@ -130,10 +148,12 @@ module.exports = !global.ZeresPluginLibrary ? class {
 				convertedInstructions.forEach((e) => {
 					cssKeyframes += `${e[0]}%{content:"${e[1]}"}`
 				})
+				let classes = ["activity-2EQDZv", "customStatus-3XAoF9", "customStatus-kFfkj5"];
 
-				css += `.status${entry[0]} > * {display: none!important;} .status${entry[0]}::before {content:"${entry[1].thumb}";animation:a${entry[0]} ${entry[1].sum}s linear infinite} @keyframes a${entry[0]}{${cssKeyframes}}`
+				classes = classes.map(e => `[data-user-id="${entry[0]}"] .${e}`);
+				css += `.status${entry[0]}>*, ${classes.map(e => e + ">*").join(",")} {display: none!important;}.status${entry[0]}::before, ${classes.map(e => e + "::before").join(",")} {content:"${entry[1].thumb}";animation:frames${entry[0]} ${entry[1].sum}s linear infinite} @keyframes frames${entry[0]}{${cssKeyframes}}`
 			});
-			console.log(css)
+			//console.log(css);
 			return css;
 		}
 
@@ -144,3 +164,7 @@ module.exports = !global.ZeresPluginLibrary ? class {
 
 	}
 })(global.ZeresPluginLibrary.buildPlugin(config));
+
+
+//[data-user-id="123456789"] .activity-2EQDZv::before,[data-user-id="123456789"] .customStatus-3XAoF9::before,[data-user-id="123456789"] .customStatus-kFfkj5::before
+
